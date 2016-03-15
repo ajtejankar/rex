c32a65921422e82cfb04a0fad40a96870146bf2c
Ajinkya Tejankar <tejankarajinkya@gmail.com>
2016-03-15 22:16:32 +0530

    feat(index): Add `match` api
    Also re-factor the test cases for `test` api

--- src/index.js
+++ src/index.js
@@ -16,4 +16,32 @@ export default class Rex {
 
     return simulator.done;
   }
+
+  match(input) {
+    let simulator = new Simulator(this.nfa), start, i, end;
+
+    for (i = 0; i < input.length; i++) {
+      if (simulator.match(input[i])) {
+        start = start === undefined ? i : start;
+
+        if (simulator.done) end = i;
+      }
+      else if (end !== undefined) {
+        break;
+      }
+      else {
+        i = start === undefined ? i : start;
+        start = undefined;
+        simulator = new Simulator(this.nfa);
+      }
+    }
+
+    end = end === undefined ? i : end;
+
+    if (start !== undefined && end < input.length) {
+      return input.slice(start, end + 1);
+    }
+
+    return null;
+  }
 }
--- test/index.js
+++ test/index.js
@@ -5,24 +5,55 @@ describe('Regular expression test api', () => {
   it('should handle primitive operations', () => {
     let re = new Rex('ab[cd][^ef]');
 
-    assert.equal(re.test('abdg'), true);
-    assert.equal(re.test('abcghi'), false);
-    assert.equal(re.test('amp'), false);
+    assert.isTrue(re.test('abdg'));
+    assert.isFalse(re.test('abcghi'));
+    assert.isFalse(re.test('amp'));
   });
 
   it('should handle unary operators', () => {
     let re = new Rex('a*b?[cd]?[^ef]*[gh]+i+');
 
-    assert.equal(re.test('gi'), true);
-    assert.equal(re.test('xpxaaabmmghaaabmmghi'), true);
-    assert.equal(re.test('abghi'), true);
-    assert.equal(re.test('ghghii'), true);
-    assert.equal(re.test('egi'), false);
+    assert.isTrue(re.test('gi'));
+    assert.isTrue(re.test('xpxaaabmmghaaabmmghi'));
+    assert.isTrue(re.test('abghi'));
+    assert.isTrue(re.test('ghghii'));
+    assert.isFalse(re.test('egi'));
   });
 
   it('should handle grouping of sub-expressions operators', () => {
     let re = new Rex('a(bc*[de]?)+');
 
-    assert.equal(re.test('abcdbcebbc'), true);
+    assert.isTrue(re.test('abcd'));
+    assert.isTrue(re.test('abcdbcebbc'));
+    assert.isFalse(re.test('amdp'));
+  });
+});
+
+describe('Regular expression match api', () => {
+  it('should handle primitive operations', () => {
+    let re = new Rex('ab[cd][^ef]');
+
+    assert.equal(re.match('abdg'), 'abdg');
+    assert.equal(re.match('xababceabcp'), 'abcp');
+    assert.equal(re.match('xababceabcpmda'), 'abcp');
+    assert.equal(re.match('xman'), null);
+  });
+
+  it('should handle unary operators', () => {
+    let re = new Rex('a*b?[^ef]*[gh]+');
+
+    assert.equal(re.match('gh'), 'gh');
+    assert.equal(re.match('xyxygh'), 'xyxygh');
+    assert.equal(re.match('abxyxyghp'), 'abxyxygh');
+    assert.equal(re.match('eeabxyxyghp'), 'abxyxygh');
+    assert.equal(re.match('triptoport'), null);
+  });
+
+  it('should handle sub-expressions grouping', () => {
+    let re = new Rex('a(bc*[de]?)+');
+
+    assert.equal(re.match('abcd'), 'abcd');
+    assert.equal(re.match('xpaabcdbce'), 'abcdbce');
+    assert.equal(re.match('xpaaa'), null);
   });
 });
